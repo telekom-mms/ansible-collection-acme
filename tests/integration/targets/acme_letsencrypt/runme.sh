98e1c47d537cc5f4abcf539743d2f1eba2ac3a66
Sebastian Gumprich <sebastian.gumprich@t-systems.com>
2021-05-18 16:43:46 +0200

    revert to local testing of hetzner

--- tests/integration/targets/acme_letsencrypt/runme.sh
+++ tests/integration/targets/acme_letsencrypt/runme.sh
@@ -3,5 +3,4 @@
 set -eux
 
 ansible-playbook dns-challenge-pebble.yml
-ansible-playbook dns-challenge-hetzner.yml -e acme_hetzner_auth_token=$HETZNER_AUTH_TOKEN
 ansible-playbook http-challenge-local.yml
