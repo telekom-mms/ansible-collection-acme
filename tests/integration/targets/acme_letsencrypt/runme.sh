de8c3974a3e06b0486014d25e182294e412b30b6
Sebastian Gumprich <sebastian.gumprich@t-systems.com>
2021-05-18 14:25:34 +0200

    try to fix secret for hetzner

--- tests/integration/targets/acme_letsencrypt/runme.sh
+++ tests/integration/targets/acme_letsencrypt/runme.sh
@@ -3,5 +3,5 @@
 set -eux
 
 ansible-playbook dns-challenge-pebble.yml
-ansible-playbook dns-challenge-hetzner.yml
+ansible-playbook dns-challenge-hetzner.yml -e acme_hetzner_auth_token=$HETZNER_AUTH_TOKEN
 ansible-playbook http-challenge-local.yml
