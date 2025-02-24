6767a906244af61a4a8e68907144fd3dd089dbe8
Michael Kluge <m.kluge@t-systems.com>
2025-02-24 15:13:23 +0100

    Fixes after last commit

--- plugins/filter/find_challenges.py
+++ plugins/filter/find_challenges.py
@@ -15,15 +15,15 @@ def find_challenges(challenge_response: dict, challenge_type: str, expected_doma
 
 
 def build_challenges(challenge_response: dict, challenge_type: str, challenge_data_type: str = 'default') -> dict:
-    assert challenge_type in ['http-01', 'dns-01']
+    assert challenge_type in ['http-01', 'dns-01'], f"Unknown challenge_type ({challenge_type})"
 
     if challenge_data_type == 'default':
         assert 'challenge_data' in challenge_response
         return extract_challenges_from_challenge_data(challenge_response, challenge_type)
 
     elif challenge_data_type == 'telesec':
-        assert 'challenge_data' in challenge_response
-        assert 'authorizations' in challenge_response
+        assert 'challenge_data' in challenge_response, "'challenge_data' missing in API response"
+        assert 'authorizations' in challenge_response, "'authorizations' missing in API response"
         if len(challenge_response['challenge_data']) > 0:
             return extract_challenges_from_challenge_data(challenge_response, challenge_type)
         else:
@@ -36,7 +36,8 @@ def extract_challenges_from_challenge_data(challenge_response: dict, challenge_t
     re = {}
     for domain, domain_data in challenge_response['challenge_data'].items():
         assert challenge_type in domain_data
-        re[domain] = domain_data[challenge_type]
+        re[domain] = {}
+        re[domain][challenge_type] = domain_data[challenge_type]
 
     return re
 
@@ -44,25 +45,27 @@ def extract_challenges_from_challenge_data(challenge_response: dict, challenge_t
 def extract_challenges_from_authorizations_data(challenge_response: dict, challenge_type: str):
     re = {}
     for domain, domain_data in challenge_response['authorizations'].items():
-        assert 'challenges' in challenge_response['authorizations'][domain]
+        assert 'challenges' in challenge_response['authorizations'][domain], f"'challenges' missing for '{domain}'"
 
         for challenge in challenge_response['authorizations'][domain]['challenges']:
             if challenge['type'] == challenge_type:
+                re[domain] = {}
                 if challenge_type == 'dns-01':
-                    re[domain] = build_challenge_data_from_authorizations_dns01(challenge['token'], domain)
+                    re[domain][challenge_type] = build_data_from_authorizations_dns01(challenge['token'], domain)
                 elif challenge_type == 'http-01':
-                    re[domain] = build_challenge_data_from_authorizations_http01(challenge['token'], domain)
+                    re[domain][challenge_type] = build_data_from_authorizations_http01(challenge['token'], domain)
     return re
 
 
-def build_challenge_data_from_authorizations_dns01(token: str, domain: str) -> dict:
+def build_data_from_authorizations_dns01(token: str, domain: str) -> dict:
     return {
         "record": f"_acme-challenge.{domain}",
         "resource": "_acme-challenge",
         "resource_value": token
     }
 
-def build_challenge_data_from_authorizations_http01(token: str, domain: str) -> dict:
+
+def build_data_from_authorizations_http01(token: str, domain: str) -> dict:
     return {
         "resource": f".well-known/acme-challenge/{token}",
         "resource_value": token
@@ -71,7 +74,8 @@ def build_challenge_data_from_authorizations_http01(token: str, domain: str) -> 
 
 class FilterModule(object):
     """ utility filters for operating on dictionary """
-    def filters(self):
+    @staticmethod
+    def filters():
         return {
             'find_challenges': find_challenges
         }
