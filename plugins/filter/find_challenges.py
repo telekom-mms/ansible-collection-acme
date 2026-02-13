5e07b3464a7cc84829dd92c8505d84dba7501768
ivro <ivo.roentzsch@telekom.de>
2026-02-13 13:08:14 +0100

    fix wildcard challenges if the root-domain is used as subject_alt_name

--- plugins/filter/find_challenges.py
+++ plugins/filter/find_challenges.py
@@ -19,6 +19,8 @@ def find_challenges(challenge_response: dict, challenge_type: str, expected_doma
     errors = []
 
     if domains_in_response == set(expected_domains):
+        if challenge_type == 'dns-01':
+            handle_wildcard_challenge_data_dns01(challenges)
         return challenges
     else:
         expected_not_found = set(expected_domains).difference(domains_in_response)
@@ -112,13 +114,59 @@ def build_data_from_authorizations_dns01(token: str, domain: str) -> dict:
 
 def build_data_from_authorizations_http01(token: str, domain: str) -> dict:
     """
-    build the fields that one would expect out of challange_data for data coming out of authorizations (for HTTP-01)
+    build the fields that one would expect out of challenge_data for data coming out of authorizations (for HTTP-01)
     """
     return {
         "resource": f".well-known/acme-challenge/{token}",
         "resource_value": token
     }
 
+def handle_wildcard_challenge_data_dns01(challenges: dict) -> dict:
+    """
+    merges the challenge_data if the root domain is defined as an alternative_name for a wildcard certificate (for DNS-01)
+    after a merge, resource_value is returned as a list
+    """
+    domains_to_remove = []
+    domains_to_add = {}
+
+    wildcard_domains = [domain for domain in challenges.keys() if domain.startswith("*.")]
+
+    for wildcard_domain in wildcard_domains:
+        root_domain = wildcard_domain.replace("*.", "")
+        if root_domain in challenges:
+            root_entry = challenges[root_domain]
+            wildcard_resource_value = challenges[wildcard_domain]["dns-01"]["resource_value"]
+            root_resource_value = root_entry["dns-01"]["resource_value"]
+            resource_values = {wildcard_resource_value, root_resource_value}
+            if wildcard_resource_value != root_resource_value:
+                merged_entry = {
+                    "dns-01": {
+                        "record": root_entry["dns-01"]["record"],
+                        "resource": root_entry["dns-01"]["resource"],
+                        "resource_value": list(resource_values)
+                    }
+                }
+                domains_to_remove.extend([wildcard_domain, root_domain])
+                domains_to_add[root_domain] = merged_entry
+            else:
+                domains_to_remove.append(wildcard_domain)
+        else:
+            domains_to_remove.append(wildcard_domain)
+            domains_to_add[root_domain] = challenges[wildcard_domain]
+
+    return modify_challenges(challenges, domains_to_remove, domains_to_add)
+
+
+def modify_challenges(challenges: dict, domains_to_remove: list, domains_to_add: dict) -> dict:
+    """
+    remove and adds given challenge_data
+    """
+    for domain in domains_to_remove:
+        challenges.pop(domain, None)
+
+    challenges.update(domains_to_add)
+
+    return challenges
 
 class FilterModule(object):
     """ utility filters for operating on dictionary """
